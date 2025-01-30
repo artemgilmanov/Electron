31946f3d0d2b104c5a811893b6532dc32f763200
Artem Gilmanov <84340745+artemgilmanov@users.noreply.github.com>
2025-01-30 09:21:55 +0100

    Add files via upload

--- VideoInfo/index.js
+++ VideoInfo/index.js
@@ -2,6 +2,10 @@ const electron = require('electron');
 
 const { app, BrowserWindow } = electron;
 
+// const mainWindow = new BrowserWindow({
+//   webPreferences: { nodeIntegration: true, contextIsolation: false },
+// });
+
 app.on('ready', () => {
   const mainWindow = new BrowserWindow({});
   mainWindow.loadURL(`file://${__dirname}/index.html`);
