7f7ddee6169eaace5ba9999f95e2b6da90957e4f
Artem Gilmanov <84340745+artemgilmanov@users.noreply.github.com>
2025-02-02 18:58:42 +0100

    Add files via upload

--- VideoInfo/index.js
+++ VideoInfo/index.js
@@ -1,19 +1,24 @@
-const electron = require('electron');
-const ffmpeg = require('fluent-ffmpeg');
+const electron = require("electron");
+const ffmpeg = require("fluent-ffmpeg");
+const path = require("path");
 
 const { app, BrowserWindow, ipcMain } = electron;
 
 let mainWindow;
 
-app.on('ready', () => {
+app.on("ready", () => {
   mainWindow = new BrowserWindow({
-    webPreferences: { nodeIntegration: true, contextIsolation: false },
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, "preload.js"),
+    },
   });
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
 
-ipcMain.on('video:submit', (event, path) => {
-  ffmpeg.ffprobe(path, (err, metadata) => {
-    mainWindow.webContents.send('video:metadata', metadata.format.duration);
+ipcMain.on("video:submit", (event, filePath) => {
+  ffmpeg.ffprobe(filePath, (err, metadata) => {
+    event.sender.send("video:metadata", metadata.format.duration);
   });
 });
